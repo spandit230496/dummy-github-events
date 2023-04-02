71b70b1c6ad1eb178b943f0b2deaaf6981e5fb8e
spandit230496 <sandip.pandit230496@gmail.com>
2023-04-02 13:59:55 +0530

    commit

---
+++ src/main/java/com/github/webhook/controller/WebhookController.java
@@ -0,0 +1,21 @@
+package com.github.webhook.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/webhook")
+public class WebhookController {
+    public ResponseEntity<String>print(@RequestBody String requestBody){
+        try{
+            System.out.println("log"+requestBody);
+        }
+        catch (Exception e){
+            return new ResponseEntity<>("Bad Request", HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(requestBody, HttpStatus.BAD_REQUEST);
+    }
+}
