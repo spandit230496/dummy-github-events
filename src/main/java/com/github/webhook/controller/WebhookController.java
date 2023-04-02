f9884010b9d7cc23d9292e99075d4b1f066f421c
spandit230496 <sandip.pandit230496@gmail.com>
2023-04-02 14:07:31 +0530

    push

--- src/main/java/com/github/webhook/controller/WebhookController.java
+++ src/main/java/com/github/webhook/controller/WebhookController.java
@@ -2,6 +2,7 @@ package com.github.webhook.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/webhook")
 public class WebhookController {
+
+    @PostMapping
     public ResponseEntity<String>print(@RequestBody String requestBody){
         try{
             System.out.println("log"+requestBody);
